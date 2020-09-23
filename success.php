7f27698f2cabebe1236d26dd86746a8563c860a1
sphaneul <63492821+sphaneul@users.noreply.github.com>
2020-09-23 22:38:05 +0900

    Add files via upload

---
+++ success.php
@@ -0,0 +1,11 @@
+<?php $id = $_POST['userid'];
+$pw = $_POST['userpw'];
+$name = $_POST['username'];
+$gender = $_POST['usergender'];
+$email = $_POST['useremail'];
+
+echo "아이디 : $id<br>";
+echo "비밀번호 : $pw <br>";
+echo "이름 : $name <br>";
+echo "성별 : $gender <br>";
+?>
