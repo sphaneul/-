5057825e1d27defe2974579b7804acc91589e36e
sphaneul <63492821+sphaneul@users.noreply.github.com>
2020-09-23 22:40:10 +0900

    Update success.php

--- success.php
+++ success.php
@@ -7,6 +7,6 @@ $gender = $_POST['usergender'];
 echo "아이디 : $id<br>";
 echo "비밀번호 : $pw <br>";
 echo "이름 : $name <br>";
-echo "성별 : $email <br>";
+echo "이메일 : $email <br>";
 echo "성별 : $gender <br>";
 ?>
